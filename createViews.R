1e9128dc011cbc6b5ca84ad578a11715ac7cd634
sampcarlson <sam.p.carlson@gmail.com>
2023-03-22 13:16:56 -0600

    add location name to locationattributes view

--- createViews.R
+++ createViews.R
@@ -13,8 +13,8 @@ dbSendQuery(conn, "DROP MATERIALIZED VIEW locationattributes")
 #                     LEFT JOIN data ON locations.locationid = data.locationid GROUP BY locations.locationid, watersheds.geometry")
 
 
-dbSendQuery(conn, "CREATE MATERIALIZED VIEW locationattributes AS SELECT ROW_NUMBER() OVER(), met.locationid, met.locationgeometry, met.metrics, wsh.wshedareakm, wsh.watershedgeometry FROM
-                      (SELECT DISTINCT locations.locationid, locations.geometry AS locationgeometry, STRING_AGG(DISTINCT(data.metric), ',') AS metrics FROM locations LEFT JOIN data ON locations.locationid = data.locationid GROUP BY locations.locationid) met
+dbSendQuery(conn, "CREATE MATERIALIZED VIEW locationattributes AS SELECT ROW_NUMBER() OVER(), met.locationid, met.name, met.locationgeometry, met.metrics, wsh.wshedareakm, wsh.watershedgeometry FROM
+                      (SELECT DISTINCT locations.name, locations.locationid, locations.geometry AS locationgeometry, STRING_AGG(DISTINCT(data.metric), ',') AS metrics FROM locations LEFT JOIN data ON locations.locationid = data.locationid GROUP BY locations.locationid) met
                      LEFT JOIN 
                       (SELECT locations.locationid, watersheds.geometry AS watershedgeometry, ST_AREA(watersheds.geometry)/1000000 AS wshedareakm FROM locations LEFT JOIN watersheds ON locations.locationid = watersheds.outflowlocationid) wsh
                       ON met.locationid = wsh.locationid;")
